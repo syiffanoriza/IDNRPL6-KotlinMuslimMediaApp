14bb3983e2171ebaa62af25bacbb1419c21bfcd6
syiffanoriza <syiffa.noriza@gmail.com>
2023-10-16 09:47:11 +0700

    Project Update

--- app/src/main/java/com/nori/muslimmediaapp/ui/MainActivity.kt
+++ app/src/main/java/com/nori/muslimmediaapp/ui/MainActivity.kt
@@ -2,13 +2,40 @@ package com.nori.muslimmediaapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nori.muslimmediaapp.R
+import com.nori.muslimmediaapp.adapter.SectionPagerAdapter
+import com.nori.muslimmediaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _bindng: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
+    }
+
+    private fun setUpViewPager() {
+        binding.vpNews.adapter = SectionPagerAdapter(this)
+
+        val tabList = arrayOf(
+            "Common",
+            "About Al Quran",
+            "Al Jazeera",
+            "Warn for Muslim"
+        )
+        TabLayoutMediator(binding.tabs, binding.vpNews) {
+            tab, position -> tab.text = tabList[position]
+        }.attach()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu_main, menu)
+
     }
 }
